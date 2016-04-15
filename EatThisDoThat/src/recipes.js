917eeb644909b8f3426520d1bb85bbf0ec266187
fonzinj1 <fonzinj1@tcnj.edu>
2016-04-15 10:45:40 -0400

    Eat This, Do That Amazon Alexa Skill

---
+++ EatThisDoThat/src/recipes.js
@@ -0,0 +1,59 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+
+    "whole milk": "Whole milk has about 150 calories. To burn that off, you can go hiking for 26 minutes, light walking for 1 hour and 2 minutes, or fast walking for 26 minutes.",
+    "chocolate milk": "Chocolate milk has about 160 calories. To burn that off, you can jog for 45 minutes, run for 22 minutes, or play outside for 57 minutes.",
+    "milkshake": "Milkshakes can have about 500 to 600 calories. To burn that off, you can ride a bike for 2 hours and 25 minutes, play baseball or softball for 1 hour and 52 minutes, or dance for 1 hour and 44 minutes.",
+    "Iced Coffee": "Some small iced coffees can have about 150 to 200 calories. To burn that off, you can swim for 26 minutes, jump on a trampoline for 48 minutes, or play soccer for 21 minutes.",
+    "small soda": "Some small sodas can have about 180 calories or more. To burn that off, you can play basketball for 30 minutes, jump rope for 20 minutes, or lightly walk for 1 hour and 12 minutes.",
+    "medium soda": "some medium sided sodas can have about 240 calories. To burn that off, you can fast walk for 40 minutes, jog for 38 minutes, or play outside for 1 hour and 25 minutes.",
+    "large soda": "some large sodas can have about 300 or more calories. To burn that off, you can ride a bike for 1 hour and 30 minutes, do some household chores for 1 hour and 30 minutes, or dance for 1 hour and 5 minutes.",
+    "water": "Water has no calories. Drink as much as you want.",
+    "apple": "A medium apple, which is about the size of your fist, has about 80 calories. To burn that off, you can dance for 17 minutes, swim for 14 minutes, or play soccer for 11 minutes.",
+    "banana": "A medium banana has about 30 calories. To burn that off, you can play basketball for 4 minutes, jump rope for 3 minutes, or do outside chores for 5 minutes.",
+    "watermelon": "one slice of watermelon has about 22 calories. To burn that off, you can hike for 4 minutes, do yoga for 6 minutes, or garden for 5 minutes.",
+    "orange": "One medium orange has about 60 calories. To burn that off you can, play active videogames for 31 minutes, walk for 25 minutes, or jog for 10 minutes.",
+    "carrot": "A handful or so of carrots has about 30 calories. To burn that off, you can jog for 5 minutes, play outside for 11 minutes, or play baseball for 7 minutes.",
+    "broccoli": "A handful of broccoli has about 50 calories. To burn that off, you can dance for 11 minutes, roller skate for 10 minutes, or jump rope for 6 minutes.",
+    "salad": "One and a half cups of tossed salad with no dressing has about 33 calories. To burn that off, you can jump rope for 4 minutes, hike for 6 minutes, or do yoga for 8 minutes.",
+    "peanut butter": "Two tablespoons of peanut butter has about 200 calories. To burn that off, you can garden for 45 minutes, fast walk for 33 minutes, or run for 27 minutes.",
+    "spinach": "One cup of spinach has about 7 calories: To burn that off, you can run for 1 minute, play outside for 2 minutes, or jump in place for 1 minute.",
+    "cake": "One slice of cake with frosting has about 250 calories. To burn that off, you can jump rope for 26 minutes, do gymnastics for 1 hour, or climb stairs for 55 minutes.", 
+    "ice cream": "one scoop of ice cream has about 150 calories. To burn that off, you can jog for 25 minutes, ride a bike for 42 minutes, or dance for 30 minutes.",
+    "fat free yogurt": "one small container of fat free yogurt has about 140 calories. To burn that off, you can swim for 25 minutes, jump in place for 45 minutes, or play basketball for 23 minutes.", 
+    "granola bar": "Most small granola bars have about 150 to 200 calories. To burn that off, you can play kickball for 40 minutes, run for 30 minutes, or walk for 52 minutes.",
+    "candy bar": "Some large candy bars have about 210 calories. To burn that off, you can play basketball for 35 minutes, jump in place for 23 minutes, or do outside chores for 42 minutes.",
+    "movie popcorn": "Popcorn that you get at the movie theater can be almost 800 calories. To burn that off, you can walk for 4 hours and 48 minutes, run for 1 hour and 39 minutes, or play outside for 4 hours and 17 minutes.",
+    "plain popcorn": "Four cups of plain popcorn have about 180 calories. To burn that off, you can play outside for 1 hour and 4 minutes, dance for 40 minutes, or roller skate for 35 minutes.",
+    "peanuts": "One small palm full of peanuts has about 160 calories. To burn that off, you can play on a trampoline for 52 minutes, jump rope for 18 minutes, or hike for 27 minutes.",
+    "potato chips": "one small bag of potato chips has about 150 calories. To burn that off, you can garden for 35 minutes, walk up stairs for 25 minutes, or fast walk for 30 minutes.",
+    "nachos": "a small bowl of nachos with regular cheese has about 150 calories. To burn that off, you can jog for 23 minutes, play outside for 52 minutes, or ride your bike very fast for 21 minutes.",
+    "medium fries": "a bag of medium fries has about 370 calories. To burn that off, you can do chores for 1 hour and 52 minutes, play baseball or softball for 1 hour and 27 minutes, or swim for 1 hour and 5 minutes.",
+    "large fries": "the average bag of large fries has about 500 calories or more. To burn that off, you can play soccer for 1 hour and 8 minutes, jump rope for 54 minutes, or play volleyball for 2 hours and 19 minutes.",
+    "bagel": "One medium bagel has about 300 calories. To burn that off, you can play Frisbee for 1 hour and 52 minutes, play kickball for 56 minutes, or jump rope for 35 minutes.",
+    "bagel with cream cheese": "One medium bagel with a thin layer of cream cheese has about 400 calories. To burn that off, you can jump rope for 45 minutes, do yoga for 1 hour and 40 minutes, or garden for 1 hour and 26 minutes.",
+    "chicken nuggets": "5 pieces of chicken nuggets with no sauce have about 240 calories. To burn that off, you can walk for 1 hour 35 minutes, jog for 38 minutes, or play outside for 1 hour and 25 minutes.",
+    "hotdog": "One hotdog has about 245 calories. To burn that off, you can play outside for 1 hour and 26 minutes, fast bike for 35 minutes, or play baseball or softball for 57 minutes.",
+    "cheeseburger": "One medium cheeseburger has about 300 to 400 calories. To burn that off, you can walk upstairs for 1 hour and 5 minutes, do yoga for 2 hours and 15 minutes, or dance for 1 hour and 40 minutes.",
+    "hamburger": "1 medium hamburger has about 250 calories. To burn that off, you can dance for 54 minutes, swim for 44 minutes, or jump on a trampoline for 1 hour and 21 minutes.",
+    "turkey sandwich": "One turkey sandwich, depending on the type of bread and toppings, can have about 300 calories. To burn that off, you can wash a car for 1 hour and 15 minutes, play soccer for 43 minutes, or jump rope for 35 minutes.",
+    "peanut butter and jelly sandwich": "one P B and J sandwich has about 380 calories. To burn that off, you can play volleyball for 1 hour and 50 minutes, do gymnastics for 1 hour and 34 minutes, or fast walk for 1 hour and 4 minutes.",
+    "bean burrito": "The average bean burrito can have about 380 calories. To burn that off, you can fast walk for 1 hour and 4 minutes, jog for 1 hour, or play outside for 2 hours and 15 minutes.",
+    "taco": "the average taco, depending on the type of meat and toppings, has about 160 calories. To burn that off, you can play outside for 56 minutes, do household chores for 47 minutes, or dance for 34 minutes.",
+    "cheese pizza": "One slice of plain cheese pizza has about 260 calories. To burn that off, you can dance for 57 minutes, swim for 46 minutes, or jump on a trampoline for 1 hour and 24 minutes.",
+    "pepperoni pizza": "One slice of pepperoni pizza has about 330 calories. To burn that off, you can play soccer for 46 minutes, play basketball for 55 minutes, or jump rope for 37 minutes.",
+    "mac and cheese": "A half cup of mac and cheese has about 180 calories. To burn that off, you can jump rope for 19 minutes, play volleyball for 50 minutes, or hike for 30 minutes.",
+    "apple juice": "the average bottle of apple juice has about 114 calories. To burn that off, you can do yoga for 28 minutes, garden for 25 minutes, or walk for 46 minutes.",
+    "orange juice": "The average bottle of orange juice has about 122 calories. To burn that off, you can walk for 49 minutes, jog for 19 minutes, or run for 17 minutes."
+
+
+};
